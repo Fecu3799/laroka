af5df94df5a62df3a1ea1150d9461c2e9b925ec4
Facundo Reynoso <facurey.arg@gmail.com>
2026-06-19 17:41:21 -0300

    feat: US-SEC-02 CSP backoffice

--- backoffice/src/main.jsx
+++ backoffice/src/main.jsx
@@ -1,5 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// US-SEC-02: fuentes self-hosted (antes @import remoto a fonts.googleapis.com).
+// Servidas same-origin para cumplir la CSP estricta del backoffice sin font-src
+// externo. Pesos exactos que usaba el @import original.
+import '@fontsource/barlow-condensed/600.css'
+import '@fontsource/barlow-condensed/700.css'
+import '@fontsource/barlow-condensed/800.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/700.css'
+import '@fontsource/jetbrains-mono/800.css'
 import './index.css'
 import App from './App.jsx'
 
