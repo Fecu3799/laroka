31035e0a86c2884e04fdeb35761f3135c83afece
Facundo Reynoso <facurey.arg@gmail.com>
2026-06-20 18:29:31 -0300

    feat: US-SEC-03 CSP client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/oswald/400.css'
+import '@fontsource/oswald/500.css'
+import '@fontsource/oswald/700.css'
+import '@fontsource/barlow-condensed/700.css'
+import '@fontsource/barlow-condensed/800.css'
+import '@fontsource/inria-sans/400.css'
 import './index.css'
 import App from './App.jsx'
 
