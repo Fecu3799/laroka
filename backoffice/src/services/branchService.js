f71659db743e1cb24b4c815859ead379793ae0ba
Facu <facurey.arg@gmail.com>
2026-06-27 02:24:18 +0000

    fix: backoffice tenant_id auth

--- backoffice/src/services/branchService.js
+++ backoffice/src/services/branchService.js
@@ -3,7 +3,7 @@ import { apiFetch } from './http'
 const API_URL = import.meta.env.VITE_API_URL ?? ''
 
 export async function fetchBranches(token) {
-  const res = await apiFetch(`${API_URL}/branches`, {
+  const res = await apiFetch(`${API_URL}/backoffice/branches`, {
     headers: { Authorization: `Bearer ${token}` },
   })
   return res.json()
